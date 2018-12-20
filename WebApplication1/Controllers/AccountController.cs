1caaee1e504de98e5532e0f271750567fb5eb15c
prabhjot325 <prabhjot325@users.noreply.github.com>
2018-12-20 22:38:28 +0530

    publish git

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -18,6 +18,7 @@ namespace WebApplication1.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+
         public AccountController()
         {
         }
